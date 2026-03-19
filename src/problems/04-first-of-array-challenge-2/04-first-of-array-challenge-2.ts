301bd11c2618ede522fd34da9f8176fe4cbe8c25
Zain-ul-din <78583049+Zain-ul-din@users.noreply.github.com>
2026-03-19 16:08:44 +0500

    feat: first of array challenge

--- src/problems/04-first-of-array-challenge-2/04-first-of-array-challenge-2.ts
+++ src/problems/04-first-of-array-challenge-2/04-first-of-array-challenge-2.ts
@@ -18,7 +18,7 @@ import type { Equal, Expect } from '@course/types'
 
 /* _____________ Your Code Here _____________ */
 
-type First = {};
+type First<T extends readonly any[]> = T extends [] ? never : T[0]
 
 /* _____________ Test Cases _____________ */
 

48664eeca7e3828a55c34328aa0cd22f7d298409
Zain-ul-din <78583049+Zain-ul-din@users.noreply.github.com>
2026-03-19 16:46:59 +0500

    feat: throttle challenge

--- src/problems/05-throttle/throttle.vanila.ts
+++ src/problems/05-throttle/throttle.vanila.ts
@@ -1,8 +1,20 @@
 // bun test src/problems/05-throttle/test/throttle.test.ts
 
-export function throttle() {
-
+export function throttle<T extends (...args: any[]) => void, This>(
+  fn: T,
+  delay: number,
+): (...args: any[]) => void {
+  let lastTimeCalled: Date | null = null
+  return function (this: This, ...args: Parameters<T>) {
+    const timeElapsed =
+      lastTimeCalled === null ? delay : Number(new Date()) - Number(lastTimeCalled)
+    if (timeElapsed >= delay) {
+      fn.apply(this, args)
+      lastTimeCalled = new Date()
+    }
+  }
 }
+
 // --- Examples ---
 // Uncomment to test your implementation:
 

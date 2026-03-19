828fb6adc1c58b52620172dba0fd3553c5deed17
Zain-ul-din <78583049+Zain-ul-din@users.noreply.github.com>
2026-03-19 18:19:15 +0500

    feat: es5 extends solution

--- src/problems/06-es5-extends/es5-extends.vanila.ts
+++ src/problems/06-es5-extends/es5-extends.vanila.ts
@@ -2,9 +2,20 @@
 
 export const myExtends = (SuperType: Function, SubType: Function) => {
   // Step 1: Create a new constructor function MyType(this, ...args)
+  function extendedType(...args: any[]) {
+    const target = Object.create(SubType.prototype)
+    SuperType.apply(target, args)
+    SubType.apply(target, args)
+    return target
+  }
+
   // Step 2: Set up prototype chain
+  Object.setPrototypeOf(SubType.prototype, SuperType.prototype)
   // Step 3: Set up static/constructor inheritance
+  Object.setPrototypeOf(extendedType, SuperType)
+
   // Step 4: Return MyType
+  return extendedType
 }
 
 // --- Examples ---
@@ -12,10 +23,10 @@ export const myExtends = (SuperType: Function, SubType: Function) => {
 
 // function Animal(this: any, name: string) { this.name = name }
 // Animal.prototype.greet = function () { return `Hello, ${this.name}` }
-//
+
 // function Dog(this: any) { this.breed = 'Labrador' }
 // Dog.prototype.bark = function () { return `${this.name} says Woof!` }
-//
+
 // const DogExtended = myExtends(Animal, Dog)
 // const dog = new (DogExtended as any)('Rex')
 // console.log(dog.name)    // Expected: "Rex"
